ae2abac99b45077fba6f1084319435f37a7a4401
Sudipta Das <shiningsudipto@gmail.com>
2024-02-25 21:09:06 +0600

    navbar added

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../../assets/jayga-logo.png'
+
+const Navbar = () => {
+    const navLinks = <>
+        <li><a>Why Jayga?</a></li>
+        <li><a>Item 3</a></li>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navLinks}
+                        </ul>
+                    </div>
+                    <img src={logo} alt="jayga-logo" />
+                </div>
+                <div className="navbar-center hidden lg:flex">
+
+                </div>
+                <div className="navbar-end">
+                    <ul className="menu menu-horizontal px-1">
+                        {navLinks}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar/>
             Main Layout
             <Outlet/>
         </div>
