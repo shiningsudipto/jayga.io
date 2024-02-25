a4d004b2d6dbd18c6fa0f62447416c4d00ade312
Sudipta Das <shiningsudipto@gmail.com>
2024-02-25 20:48:38 +0600

    tailwind and daisy ui setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import { router } from './routes/Router';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
